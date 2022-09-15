db6e6d19aea88fd3d92f66bf91a61bd177094b1a
lokendratanwar27 <66329843+lokendratanwar27@users.noreply.github.com>
2022-09-15 16:15:36 +0530

    Update settings.py
    connected the data to postgres

--- watchmate/settings.py
+++ watchmate/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'watchmate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangoapi',
+        'USER': 'django',
+        'PASSWORD': 'djangoadmin',
     }
 }
 
